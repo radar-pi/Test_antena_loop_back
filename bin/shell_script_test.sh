aa255ebb7d29fe18ee5682021e8647dd64f59df1
Filipe de Souza Freitas <filipe.desouzafreitas@gmail.com>
2019-04-19 12:10:42 -0300

    Adicionando script como opção para configurar

--- bin/shell_script_test.sh
+++ bin/shell_script_test.sh
@@ -2,35 +2,28 @@
 
 
 echo "Iniciando script"
-echo"Verifiando se há dispositivo conectado"
+
+echo "Verifiando se há dispositivo conectado..."
 
 bladeRF-cli -p 1>/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "No bladeRF devices connected." >&2
     exit 1
+else
+    echo "Carregando imagem hosted na pasta bin..."
+
+    bladeRF-cli -l hostedx115-latest.rbf
 fi
 
-# Lê o número serial  do dispositivo.
-DEVICE_SERIAL=$(echo "$DEVICE_INFO" | grep 'Serial #:' | sed -e 's/.*Serial #:\s\+//') 
+bladeRF-cli -e 'info' -e 'print'
+#bladeRF-cli -e 'set loopback bb_txlpf_rxlpf' -e 'print loopback'
 
-# Verifica se o dispositivo tem um codigo carregado na fpga, e.g hosted.
-DEVICE_FPGA_LOADED=$(echo "$DEVICE_INFO" | grep "FPGA loaded:" | sed -e 's/.*FPGA loaded:\s\+//')
-
-# Lê o tamanho da fpga.
-DEVICE_FPGA_SIZE=$(echo "$DEVICE_INFO" | grep "FPGA size:" | sed -e 's/.*FPGA size:\s\+//')
-
-echo ""
-echo " bladeRF encontrada"
-echo "    Serial #:    $DEVICE_SERIAL"
-echo "    FPGA type:   $DEVICE_FPGA_SIZE"
-echo "    FPGA loaded: $DEVICE_FPGA_LOADED"
-echo ""
 
 # TX
-#bladeRF-cli -e 'tx config n=10k file=test_tx.csv format=csv timeout=10000;tx start; tx; tx wait'
+#bladeRF-cli -e 'tx config file=data_shell/test_tx.csv format=csv timeout=10000 repeat=20;tx start; tx; tx wait'
 
 # RX 
-bladeRF-cli -e 'rx config n=10k file=test_rx.csv format=csv timeout=10000;rx start;rx wait'
+bladeRF-cli -e 'rx config n=10k file=data_shell/test_rx.csv format=csv timeout=10000;rx start;rx wait'
 
 
 
